00f6aad913bdb6104edb0a3c62be24ae415cac41
Otávio <otavio@leanonsystems.com>
2021-09-08 15:38:51 -0300

    parts 4 and 5 done

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,14 +2,25 @@ import React from "react"
 import Paragraph from './Paragraph'
 
 export default ({ good, neutral, bad, getAverage, getPositive }) => {
-    return (
-        <div>
-            <Paragraph text="Current statistics:" />
-            <Paragraph text={"good: " + String(good)} />
-            <Paragraph text={"neutral: " + String(neutral)} />
-            <Paragraph text={"bad: " + String(bad)} />
-            <Paragraph text={"average: " + String(getAverage)} />
-            <Paragraph text={"positive: " + String(getPositive)} />
-        </div>
-    )
+    const shouldRender = (good + neutral + bad) !== 0
+    
+    if (shouldRender) {
+        return (
+            <div>
+                <Paragraph text="Current statistics:" />
+                <Paragraph text={"good: " + String(good)} />
+                <Paragraph text={"neutral: " + String(neutral)} />
+                <Paragraph text={"bad: " + String(bad)} />
+                <Paragraph text={"average: " + String(getAverage)} />
+                <Paragraph text={"positive: " + String(getPositive)} />
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Paragraph text="Current statistics:" />
+                <Paragraph text="sorry, you need to give us feedback before seeing the results!" />
+            </div>
+        )
+    }   
 }
